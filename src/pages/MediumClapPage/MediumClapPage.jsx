43072133172cf6e3a2ce0aafc34e22004ab95fe8
Josh Bryson <josh@bryson.me.uk>
2020-04-12 23:03:15 +0100

    Initial styling for default component implemented

--- src/pages/MediumClapPage/MediumClapPage.jsx
+++ src/pages/MediumClapPage/MediumClapPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import './MediumClapPage.scss';
+
+import DefaultClap from '../../components/MediumClap/DefaultClap';
 
 const MediumClapPage = () => {
   return (
-    <div>
-      Medium Clap
+    <div className="clap-container">
+      <div className="clap-container-columns">
+        <div className="clap-container-card">
+          <h1 className="clap-container-card-heading-before">Before</h1>
+          <DefaultClap />
+        </div>
+        <div className="clap-container-card">
+          <h1 className="clap-container-card-heading-after">After</h1>
+          <DefaultClap />
+        </div>
+      </div>
     </div>
   )
 }
